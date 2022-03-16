e1561bba795f8a5b4eca6c539c1658303634c779
Evan Herman <evan.m.herman@gmail.com>
2022-03-16 18:29:44 -0400

    Tweak header

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
  * @version 1.0.0
  */
 /*
-Plugin Name: Brancher
-Plugin URI: none
-Description: A plugin that when activated shows the current branch of other plugins.
-Author: Ben Rothman
-Version: 1.0.0
-Author URI: https://benrothman.org
-*/
+ * Plugin Name: Brancher
+ * Plugin URI: none
+ * Description: A plugin that when activated shows the current branch of other plugins.
+ * Author: Ben Rothman
+ * Version: 1.0.0
+ * Author URI: https://benrothman.org
+ */
 
 /**
  * Display the Plugins git branch on the plugins.php plugin table
