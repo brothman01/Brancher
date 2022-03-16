837d193c55dc02875b9d6c8693fd41dfe50c7ece
Ben <nicole@Nicoles-MBP.home>
2022-03-16 18:20:12 -0400

    loops through each plugin and gets the path and tries to read the file but nothing.

--- index.php
+++ index.php
@@ -12,9 +12,27 @@ Version: 1.0.0
 Author URI: https://benrothman.org
 */
 add_action('wp_enqueue_scripts', 'qg_enqueue');
-function qg_enqueue() {
-    wp_enqueue_script(
-        'qgjs',
-        plugin_dir_url(__FILE__).'branch.js'
-    );
+function filter_plugin_name( $plugins ) {
+
+	$screen = get_current_screen();
+
+	if ( is_null( $screen ) || 'plugins' !== $screen->base ) {
+
+		return $plugins;
+
+	}
+
+	foreach ( $plugins as $path => &$data ) {
+
+		$path = substr( $path, 0, strpos( $path, '/' ) ) . '/.git/HEAD';
+
+		$branch = file_get_contents( $path );
+
+		$data['Name'] = $data['Name'] . ' (branch: ' . $branch . ')';
+
+	}
+
+	return $plugins;
+
 }
+add_filter( 'all_plugins', 'filter_plugin_name', PHP_INT_MAX );
